92c291a27a7392394758432f5993dfa4aaada742
MiguelPeralvo <miguel.peralvo@gmail.com>
2020-07-03 13:11:57 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='test_github_actions',
+    packages=find_packages(),
+    version='0.1.0',
+    description='test_github_actions',
+    author='Miguel Peralvo',
+    license='MIT',
+)
