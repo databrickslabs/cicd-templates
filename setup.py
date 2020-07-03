f9f40931dfc17e52282c398e86bbb2f7410e60e1
MiguelPeralvo <miguel.peralvo@gmail.com>
2020-07-03 13:03:49 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='test_azure_devops',
+    packages=find_packages(),
+    version='0.1.0',
+    description='test_azure_devops',
+    author='Miguel Peralvo',
+    license='MIT',
+)
