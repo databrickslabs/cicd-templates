c93d3ac798ded0be88aa5609f3160e52d7dc9f8a
renardeinside <polarpersonal@gmail.com>
2020-10-22 15:04:05 +0200

    fix sample integration test

--- {{cookiecutter.project_slug}}/tests/integration/sample_test.py
+++ {{cookiecutter.project_slug}}/tests/integration/sample_test.py
@@ -13,7 +13,7 @@ class SampleJobIntegrationTest(unittest.TestCase):
         }
 
         self.job = SampleJob(conf=self.test_config)
-        self.dbutils = DBUtils(self.job.spark.sparkContext)
+        self.dbutils = DBUtils(self.job.spark)
         self.spark = self.job.spark
 
     def test_sample(self):
