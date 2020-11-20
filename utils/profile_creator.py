6534ed8c004085ac82b863287a4b967fcdd651a5
cicd-templates-test <73405682+cicd-templates-test@users.noreply.github.com>
2020-11-20 20:50:45 +0100

    trigger change

--- utils/profile_creator.py
+++ utils/profile_creator.py
@@ -11,6 +11,5 @@ def configure(profile: str, host: str, token: str):
     new_config = DatabricksConfig.from_token(host, token, False)
     update_and_persist_config(profile, new_config)
 
-
 if __name__ == '__main__':
     configure()
