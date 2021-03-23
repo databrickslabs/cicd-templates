7952ae2c62cfb7e4f58ea43b60c1c9ab954e78c1
renardeinside <polarpersonal@gmail.com>
2021-03-23 22:38:15 +0100

    add initial gcp implementation

--- tests/utils.py
+++ tests/utils.py
@@ -16,7 +16,9 @@ class CicdTemplatesTest(unittest.TestCase):
     def setUp(self) -> None:
         self.test_dir = tempfile.mkdtemp()
         self.project_name = "cicd_templates_%s" % str(uuid4()).split("-")[0]
-        logging.info(f"Launching test with project name {self.project_name} and test dir {self.test_dir}")
+        logging.info(
+            f"Launching test with project name {self.project_name} and test dir {self.test_dir}"
+        )
         self.prepare_repository()
         self.project_path = Path(self.test_dir).joinpath(self.project_name)
 
@@ -25,12 +27,17 @@ class CicdTemplatesTest(unittest.TestCase):
         shutil.rmtree(self.test_dir)
 
     def prepare_repository(self):
-        cookiecutter(template=self.TEMPLATE_PATH, no_input=True, output_dir=self.test_dir, extra_context={
-            "project_name": self.project_name,
-            "cloud": "Azure",
-            "cicd_tool": "GitHub Actions",
-            "profile": "dbx-dev-azure"
-        })
+        cookiecutter(
+            template=self.TEMPLATE_PATH,
+            no_input=True,
+            output_dir=self.test_dir,
+            extra_context={
+                "project_name": self.project_name,
+                "cloud": "Azure",
+                "cicd_tool": "GitHub Actions",
+                "profile": "dbx-dev-azure",
+            },
+        )
 
     def execute_command(self, cmd):
         exit_code = os.system(cmd)
--- {{cookiecutter.project_slug}}/tests/integration/sample_test.py
+++ {{cookiecutter.project_slug}}/tests/integration/sample_test.py
@@ -1,16 +1,15 @@
 import unittest
+
 from {{cookiecutter.project_slug}}.jobs.sample.entrypoint import SampleJob
 from uuid import uuid4
-from pyspark.dbutils import DBUtils # noqa
+from pyspark.dbutils import DBUtils  # noqa
+
 
 class SampleJobIntegrationTest(unittest.TestCase):
     def setUp(self):
 
         self.test_dir = "dbfs:/tmp/tests/sample/%s" % str(uuid4())
-        self.test_config = {
-            "output_format": "delta",
-            "output_path": self.test_dir
-        }
+        self.test_config = {"output_format": "delta", "output_path": self.test_dir}
 
         self.job = SampleJob(init_conf=self.test_config)
         self.dbutils = DBUtils(self.job.spark)
@@ -21,17 +20,16 @@ class SampleJobIntegrationTest(unittest.TestCase):
         self.job.launch()
 
         output_count = (
-            self.spark
-                .read
-                .format(self.test_config["output_format"])
-                .load(self.test_config["output_path"])
-                .count()
+            self.spark.read.format(self.test_config["output_format"])
+            .load(self.test_config["output_path"])
+            .count()
         )
 
         self.assertGreater(output_count, 0)
 
     def tearDown(self):
         self.dbutils.fs.rm(self.test_dir, True)
+
 
 if __name__ == "__main__":
     # please don't change the logic of test result checks here
@@ -42,4 +40,6 @@ if __name__ == "__main__":
     runner = unittest.TextTestRunner(verbosity=2)
     result = runner.run(tests)
     if not result.wasSuccessful():
-        raise RuntimeError("One or multiple tests failed. Please check job logs for additional information.")
+        raise RuntimeError(
+            "One or multiple tests failed. Please check job logs for additional information."
+        )
